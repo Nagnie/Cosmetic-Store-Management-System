6737b2e02d98f8bd448328a60d22b5583ce81f61
22120143 <22120143@student.hcmus.edu.vn>
2024-10-27 00:26:18 +0700

    Implement LoyalCustomer class

---
+++ Cosmetic Store Management System.Core/Models/LoyalCustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmetic_Store_Management_System.Core.Models;
+public class LoyalCustomer
+{
+    public int ID { get; set; }
+
+    public string Phone { get; set; }
+
+    public string Name { get; set; }
+
+    public double Point { get; set; }
+}
