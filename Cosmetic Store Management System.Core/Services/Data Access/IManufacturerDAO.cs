2b8a79fc0eed84ff1fd28f50dcb4716586edc0f6
22120143 <22120143@student.hcmus.edu.vn>
2024-10-27 00:30:00 +0700

    Implement SQLManufacturerDAO

---
+++ Cosmetic Store Management System.Core/Services/Data Access/IManufacturerDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cosmetic_Store_Management_System.Core.Models;
+
+namespace Cosmetic_Store_Management_System.Core.Services.Data_Access;
+public interface IManufacturerDAO
+{
+    Task<List<Manufacturer>> GetManufacturers();
+    Task<Manufacturer> GetManufacturer(int id);
+    void AddManufacturer(Manufacturer manufacturer);
+    void UpdateManufacturer(Manufacturer manufacturer);
+    void DeleteManufacturer(int id);
+}
---
+++ Cosmetic Store Management System.Core/Services/Data Access/SQLManufacturerDAO.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cosmetic_Store_Management_System.Core.Helpers;
+using Cosmetic_Store_Management_System.Core.Models;
+using Npgsql;
+
+namespace Cosmetic_Store_Management_System.Core.Services.Data_Access;
+public class SQLManufacturerDAO : IManufacturerDAO
+{
+    public async void AddManufacturer(Manufacturer manufacturer)
+    {
+        NpgsqlConnection connection = DBConnection.GetConnection();
+        connection.Open();
+
+        using var command = new NpgsqlCommand();
+        command.Connection = connection;
+        command.CommandText = $"""
+                INSERT INTO MANUFACTURER (manufacturer_name, description)
+                VALUES ('{manufacturer.Name}', '{manufacturer.Description}')
+            """;
+
+        await command.ExecuteNonQueryAsync();
+
+        connection.Close();
+    }
+    public async void DeleteManufacturer(int id)
+    {
+        NpgsqlConnection connection = DBConnection.GetConnection();
+        connection.Open();
+
+        using var command = new NpgsqlCommand();
+        command.Connection = connection;
+        command.CommandText = $"""
+                DELETE FROM MANUFACTURER WHERE manufacturer_id = {id}
+            """;
+
+        await command.ExecuteNonQueryAsync();
+
+        connection.Close();
+    }
+    public async Task<Manufacturer> GetManufacturer(int id)
+    {
+        Manufacturer manufacturer = new Manufacturer();
+        NpgsqlConnection connection = DBConnection.GetConnection();
+        connection.Open();
+
+        using var command = new NpgsqlCommand();
+        command.Connection = connection;
+        command.CommandText = $"""
+                SELECT * FROM MANUFACTURER
+                WHERE manufacturer_id = {id}
+            """;
+
+        var reader = await command.ExecuteReaderAsync();
+
+        while (await reader.ReadAsync())
+        {
+            manufacturer.ID = reader.GetInt32(0);
+            manufacturer.Name = reader.GetString(1);
+            manufacturer.Description = reader.IsDBNull(2) ? null : reader.GetString(2);
+        }
+
+        connection.Close();
+        return manufacturer;
+    }
+    public async Task<List<Manufacturer>> GetManufacturers()
+    {
+        List<Manufacturer> manufacturers = new List<Manufacturer>();
+        NpgsqlConnection connection = DBConnection.GetConnection();
+        connection.Open();
+
+        using var command = new NpgsqlCommand();
+        command.Connection = connection;
+        command.CommandText = $"SELECT * FROM MANUFACTURER";
+
+        var reader = await command.ExecuteReaderAsync();
+
+        while (await reader.ReadAsync())
+        {
+            Manufacturer manufacturer = new Manufacturer();
+            manufacturer.ID = reader.GetInt32(0);
+            manufacturer.Name = reader.GetString(1);
+            manufacturer.Description = reader.IsDBNull(2) ? null : reader.GetString(2);
+            manufacturers.Add(manufacturer);
+        }
+
+        connection.Close();
+        return manufacturers;
+    }
+    public async void UpdateManufacturer(Manufacturer manufacturer)
+    {
+
+        NpgsqlConnection connection = DBConnection.GetConnection();
+        connection.Open();
+
+        using var command = new NpgsqlCommand();
+        command.Connection = connection;
+        command.CommandText = $"""
+                UPDATE MANUFACTURER
+                SET manufacturer_name = '{manufacturer.Name}', description = '{manufacturer.Description}'
+                WHERE manufacturer_id = {manufacturer.ID}
+            """;
+
+        await command.ExecuteNonQueryAsync();
+
+        connection.Close();
+    }
+}
