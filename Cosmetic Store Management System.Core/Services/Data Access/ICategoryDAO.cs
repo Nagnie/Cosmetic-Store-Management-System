5e5aa81108c1649e98aa5aa50d922f0a7718c3d7
22120143 <22120143@student.hcmus.edu.vn>
2024-10-27 00:28:33 +0700

    Implement SQLCategoryDAO

---
+++ Cosmetic Store Management System.Core/Services/Data Access/ICategoryDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cosmetic_Store_Management_System.Core.Models;
+
+namespace Cosmetic_Store_Management_System.Core.Services.Data_Access;
+public interface ICategoryDAO
+{
+    Task<List<Category>> GetCategories();
+    Task<Category> GetCategory(int ID);
+    void AddCategory(Category category);
+    void UpgradeCategory(Category category);
+    void DeleteCategory(int ID);
+}
