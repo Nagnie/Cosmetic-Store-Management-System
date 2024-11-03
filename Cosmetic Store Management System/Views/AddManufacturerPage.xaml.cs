c4e07e5fccff358185d7140f6839a984d5351c3a
22120143 <22120143@student.hcmus.edu.vn>
2024-11-03 18:10:26 +0700

    Implement: View, Insert, Update and Add Manufacturer

---
+++ Cosmetic Store Management System/Views/AddManufacturerPage.xaml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Cosmetic_Store_Management_System.ViewModels;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Cosmetic_Store_Management_System.Views;
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class AddManufacturerPage : Page
+{
+    public AddManufacturerViewModel ViewModel
+    {
+        get;
+    } = new AddManufacturerViewModel();
+
+    public AddManufacturerPage()
+    {
+        this.InitializeComponent();
+    }
+
+    private void saveButton_Click(object sender, RoutedEventArgs e)
+    {
+        ViewModel.AddManufacturer();
+        this.Frame.Navigate(typeof(ManufacturerPage));
+    }
+
+    private void cancelButton_Click(object sender, RoutedEventArgs e)
+    {
+        this.Frame.Navigate(typeof(ManufacturerPage));
+    }
+}
