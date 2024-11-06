cefd273a157348d791ed5cd8bda84c443b647b11
22120225 <tranthaongan7epct@gmail.com>
2024-11-06 15:12:57 +0700

    remove unnecessary file & update README

--- Cosmetic Store Management System/App.xaml.cs
+++ Cosmetic Store Management System/App.xaml.cs
@@ -68,8 +68,6 @@ public partial class App : Application
             services.AddSingleton<INavigationService, NavigationService>();
 
             // Core Services
-            services.AddSingleton<ISampleDataService, SampleDataService>();
-            services.AddSingleton<ISampleProductDataService, SampleProductDataService>();
             services.AddSingleton<IFileService, FileService>();
 
             // Views and ViewModels
