ba2a45fa000421d59e6ee1e1be5951245e85ce9a
22120225 <tranthaongan7epct@gmail.com>
2024-12-02 16:16:39 +0700

    fix Customer Page

--- Cosmetic Store Management System/ViewModels/CustomerViewModel.cs
+++ Cosmetic Store Management System/ViewModels/CustomerViewModel.cs
@@ -17,7 +17,10 @@ public partial class CustomerViewModel : ObservableRecipient
         get; set;
     } = new ObservableCollection<Customer>();
 
-    public ICustomerDAO dao = new SQLCustomerDAO();
+    public ICustomerDAO dao
+    {
+        get;
+    } = new SQLCustomerDAO();
 
     public string Info
     {
@@ -94,6 +97,7 @@ public partial class CustomerViewModel : ObservableRecipient
     public void LoadData()
     {
         ICustomerDAO customerDAO = new SQLCustomerDAO();
+        Customers.Clear();
 
         // Retrieve customer data from the DAO
         var (items, count) = customerDAO.GetCustomers(
@@ -101,9 +105,9 @@ public partial class CustomerViewModel : ObservableRecipient
         );
 
         // Add the retrieved customers to the collection
-        foreach (var customer in Customers)
+        foreach (var item in items)
         {
-            Customers.Add(customer);
+            Customers.Add(item);
         }
 
         if (count != TotalItems)
