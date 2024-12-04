b6cefcaf6469da348ac2cf68cb62c19b02209887
Minh Anh <22120213@student.hcmus.edu.vn>
2024-12-04 10:31:03 +0700

    Binding Image in CreateOrderPage

---
+++ Cosmetic Store Management System/ViewModels/ExtraProductListViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Media.Imaging;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Cosmetic_Store_Management_System.Core.Models;
+using Cosmetic_Store_Management_System.Core.Services.Data_Access;
+
+namespace Cosmetic_Store_Management_System.ViewModels;
+public class ExtraProductListViewModel : ObservableRecipient
+{
+    private Cosmetic _cosmetic;
+    public Cosmetic Cosmetic
+    {
+        get => _cosmetic;
+        set
+        {
+            _cosmetic = value;
+            OnPropertyChanged(nameof(Cosmetic));
+            LoadImage();  // Load the image when Cosmetic is set
+        }
+    }
+
+    private BitmapImage _image;
+    public BitmapImage Image
+    {
+        get => _image;
+        private set
+        {
+            _image = value;
+            OnPropertyChanged(nameof(Image));
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged(string propertyName) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    private void LoadImage()
+    {
+        if (Cosmetic?.ImageData != null)
+        {
+            var bitmapImage = new BitmapImage();
+            using (var stream = new MemoryStream(Cosmetic.ImageData))
+            {
+                stream.Seek(0, SeekOrigin.Begin);
+                bitmapImage.SetSourceAsync(stream.AsRandomAccessStream());
+            }
+            Image = bitmapImage;
+        }
+    }
+
+}
