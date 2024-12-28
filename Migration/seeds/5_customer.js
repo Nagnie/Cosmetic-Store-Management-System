a23a0b6dba148338d8778714accba6008da42a94
22120143 <22120143@student.hcmus.edu.vn>
2024-12-28 09:22:53 +0700

    Update seeds

--- Migration/seeds/5_customer.js
+++ Migration/seeds/5_customer.js
@@ -133,5 +133,185 @@ exports.seed = async function (knex) {
       point: 92,
       address: '02 Y Ngông, Tự An, Buôn Ma Thuột, Đắk Lắk, Việt Nam',
     },
+    {
+      customer_name: "Jory Joesbury",
+      phone: "0756139120",
+      address: "840 Vernon Point",
+      point: 219
+    },
+    {
+      customer_name: "Myrtia Creigan",
+      phone: "0593443088",
+      address: "4 Roth Street",
+      point: 110
+    },
+    {
+      customer_name: "Wenona Bradtke",
+      phone: "0802922548",
+      address: "49040 Everett Avenue",
+      point: 105
+    },
+    {
+      customer_name: "Marchelle Dyster",
+      phone: "0484744053",
+      address: "25 Dixon Place",
+      point: 14
+    },
+    {
+      customer_name: "Eward Navarre",
+      phone: "0302456348",
+      address: "593 Katie Trail",
+      point: 124
+    },
+    {
+      customer_name: "Arabele Ford",
+      phone: "0517050501",
+      address: "9 Goodland Trail",
+      point: 129
+    },
+    {
+      customer_name: "Gregory Rooson",
+      phone: "0017315107",
+      address: "99128 Knutson Street",
+      point: 266
+    },
+    {
+      customer_name: "Janie Jackling",
+      phone: "0719856782",
+      address: "5176 Towne Center",
+      point: 201
+    },
+    {
+      customer_name: "Ellswerth Gange",
+      phone: "0474755981",
+      address: "7990 Eastwood Point",
+      point: 99
+    },
+    {
+      customer_name: "Alisun Osipov",
+      phone: "0801319506",
+      address: "93 Continental Drive",
+      point: 294
+    },
+    {
+      customer_name: "Haley Trewinnard",
+      phone: "0969738547",
+      address: "01611 Becker Place",
+      point: 36
+    },
+    {
+      customer_name: "Daryl Chastang",
+      phone: "0466192771",
+      address: "16 Lighthouse Bay Pass",
+      point: 234
+    },
+    {
+      customer_name: "Jerrylee McQuarter",
+      phone: "0855275201",
+      address: "1292 Goodland Alley",
+      point: 144
+    },
+    {
+      customer_name: "Henrietta Deely",
+      phone: "0847891742",
+      address: "2 Butternut Circle",
+      point: 268
+    },
+    {
+      customer_name: "Newton Thomann",
+      phone: "0333605083",
+      address: "4 Onsgard Park",
+      point: 128
+    },
+    {
+      customer_name: "Glenden Ende",
+      phone: "0455984655",
+      address: "762 Meadow Vale Center",
+      point: 4
+    },
+    {
+      customer_name: "Michele Bumpass",
+      phone: "0485441774",
+      address: "553 Glacier Hill Park",
+      point: 15
+    },
+    {
+      customer_name: "Bertrand Disman",
+      phone: "0485098125",
+      address: "01592 Homewood Place",
+      point: 268
+    },
+    {
+      customer_name: "Oswald Kobsch",
+      phone: "0357552078",
+      address: "8 Scoville Place",
+      point: 87
+    },
+    {
+      customer_name: "Silvano Stickel",
+      phone: "0618678936",
+      address: "2 School Trail",
+      point: 123
+    },
+    {
+      customer_name: "Dunc Gergher",
+      phone: "0881217318",
+      address: "5 Brickson Park Way",
+      point: 33
+    },
+    {
+      customer_name: "Lissy Mundy",
+      phone: "0992386732",
+      address: "1 Prairie Rose Trail",
+      point: 209
+    },
+    {
+      customer_name: "Mose Carrabott",
+      phone: "0001732091",
+      address: "21291 Fieldstone Alley",
+      point: 204
+    },
+    {
+      customer_name: "Hunfredo Bodd",
+      phone: "0412818585",
+      address: "7704 Manley Junction",
+      point: 25
+    },
+    {
+      customer_name: "Jaime Fleeman",
+      phone: "0163716532",
+      address: "28 Dixon Lane",
+      point: 124
+    },
+    {
+      customer_name: "Bond Humphrys",
+      phone: "0664801931",
+      address: "98644 Brown Parkway",
+      point: 288
+    },
+    {
+      customer_name: "Nettie Attyeo",
+      phone: "0118775890",
+      address: "05822 Logan Terrace",
+      point: 275
+    },
+    {
+      customer_name: "Symon Kobsch",
+      phone: "0363234878",
+      address: "726 Messerschmidt Circle",
+      point: 280
+    },
+    {
+      customer_name: "Leta Waples",
+      phone: "0107860937",
+      address: "94402 Oak Road",
+      point: 281
+    },
+    {
+      customer_name: "Robinette Keen",
+      phone: "0716850222",
+      address: "570 Sunbrook Drive",
+      point: 224
+    }
   ]);
 };
