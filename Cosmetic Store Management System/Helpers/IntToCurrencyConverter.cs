762b769cf7db1296a1f309a123e3eb984ad48e75
Minh Anh <22120213@student.hcmus.edu.vn>
2025-01-01 23:10:22 +0700

    Currency Converter for Top 5 Customers in Analytics Page

--- Cosmetic Store Management System/Helpers/IntToCurrencyConverter.cs
+++ Cosmetic Store Management System/Helpers/IntToCurrencyConverter.cs
@@ -16,21 +16,36 @@ namespace Cosmetic_Store_Management_System.Helpers
         {
             var localSettings = ApplicationData.Current.LocalSettings;
             var unit = localSettings.Values["CurrencyUnit"].ToString();
-            var amount = (int)value;
 
+            if (value is int intValue)
+            {
+                return FormatCurrency(intValue, unit);
+            }
+            else if (value is long longValue)
+            {
+                return FormatCurrency(longValue, unit);
+            }
+            else
+            {
+                throw new InvalidCastException("Value must be of type int or long.");
+            }
+        }
+
+        private string FormatCurrency(long amount, string unit)
+        {
             if (unit == "USD")
             {
                 var usCulture = new CultureInfo("en-US");
-                var exchangeRate = (float)localSettings.Values["ExchangeRate"];
-                var USDamount = (float)(amount/exchangeRate);
-                var formattedAmount = USDamount.ToString("C2", usCulture);
-
-                return formattedAmount;
+                var exchangeRate = (float)ApplicationData.Current.LocalSettings.Values["ExchangeRate"];
+                var USDamount = (float)(amount / exchangeRate);
+                return USDamount.ToString("C2", usCulture);
             }
 
             CultureInfo culture = new CultureInfo("vi-VN");
             return amount.ToString("C0", culture);
         }
+
         public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
     }
+
 }
