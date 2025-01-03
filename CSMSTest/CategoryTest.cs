76235635a18a40f4805e8086a3b4f61488eb2444
22120143 <22120143@student.hcmus.edu.vn>
2025-01-03 14:30:17 +0700

    Add tests for updating and inserting category

--- CSMSTest/CategoryTest.cs
+++ CSMSTest/CategoryTest.cs
@@ -7,7 +7,128 @@ namespace CSMSTest
     [TestClass]
     public class CategoryTest : AppSession
     {
-        
+        [TestMethod]
+        public void AddCategory_EmptyName_Fail()
+        {
+            NavigateToCategoryPage();
+            session.FindElementByAccessibilityId("saveButton").Click();
+
+            try
+            {
+                session.FindElementByName("Lỗi");
+            }
+            catch (Exception)
+            {
+                session.FindElementByName("Error");
+            }
+
+            session.FindElementByName("OK").Click();
+        }
+
+        [TestMethod]
+        public void AddCategory_DuplicateName_Fail()
+        {
+            var time = DateTime.Now.Ticks + "";
+            NavigateToCategoryPage();
+            session.FindElementByAccessibilityId("nameInput").SendKeys("Test" + time);
+            session.FindElementByAccessibilityId("saveButton").Click();
+            session.FindElementByName("OK").Click();
+
+            session.FindElementByAccessibilityId("nameInput").SendKeys("Test" + time);
+            session.FindElementByAccessibilityId("saveButton").Click();
+            try
+            {
+                session.FindElementByName("Lỗi");
+            }
+            catch (Exception)
+            {
+                session.FindElementByName("Error");
+            }
+            session.FindElementByName("OK").Click();
+        }
+
+        [TestMethod]
+        public void AddCategory_ValidName_Success()
+        {
+            var time = DateTime.Now.Ticks + "";
+            NavigateToCategoryPage();
+            session.FindElementByAccessibilityId("nameInput").SendKeys("Test" + time);
+            session.FindElementByAccessibilityId("saveButton").Click();
+
+            try
+            {
+                session.FindElementByName("Thành công");
+            }
+            catch (Exception)
+            {
+                session.FindElementByName("Success");
+            }
+            session.FindElementByName("OK").Click();
+        }
+
+        [TestMethod]
+        public void EditCategory_EmptyName_Fail()
+        {
+            NavigateToCategoryPage();
+            session.FindElementByAccessibilityId("editButton").Click();
+            session.FindElementByAccessibilityId("nameInput").Clear();
+            session.FindElementByAccessibilityId("saveButton").Click();
+
+            try
+            {
+                session.FindElementByName("Lỗi");
+            }
+            catch (Exception)
+            {
+                session.FindElementByName("Error");
+            }
+            session.FindElementByName("OK").Click();
+        }
+
+        [TestMethod]
+        public void EditCategory_DuplicatedName_Fail()
+        {
+            var time = DateTime.Now.Ticks + "";
+            NavigateToCategoryPage();
+            session.FindElementByAccessibilityId("nameInput").SendKeys("Test" + time);
+            session.FindElementByAccessibilityId("saveButton").Click();
+            session.FindElementByName("OK").Click();
+
+            session.FindElementByAccessibilityId("editButton").Click();
+            session.FindElementByAccessibilityId("nameInput").Clear();
+            session.FindElementByAccessibilityId("nameInput").SendKeys("Test" + time);
+            session.FindElementByAccessibilityId("saveButton").Click();
+
+            try
+            {
+                session.FindElementByName("Lỗi");
+            }
+            catch (Exception)
+            {
+                session.FindElementByName("Error");
+            }
+            session.FindElementByName("OK").Click();
+        }
+
+        [TestMethod]
+        public void EditCategory_ValidName_Success()
+        {
+            var time = DateTime.Now.Ticks + "";
+            NavigateToCategoryPage();
+            session.FindElementByAccessibilityId("editButton").Click();
+            session.FindElementByAccessibilityId("nameInput").Clear();
+            session.FindElementByAccessibilityId("nameInput").SendKeys("Test" + time);
+            session.FindElementByAccessibilityId("saveButton").Click();
+            try
+            {
+                session.FindElementByName("Thành công");
+            }
+            catch (Exception)
+            {
+                session.FindElementByName("Success");
+            }
+            session.FindElementByName("OK").Click();
+        }
 
         [ClassInitialize]
         public static void ClassInitialize(TestContext context)
